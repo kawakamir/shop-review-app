030f5f9623be4e21fd33ecb6c50907ea1b9133da
kawakamir <rui.kawakami@gmail.com>
2020-09-26 00:36:01 +0900

    delete extra code at babel

--- babel.config.js
+++ babel.config.js
@@ -1,16 +1,6 @@
 module.exports = function(api) {
   api.cache(true);
   return {
-    // plugins: [
-    //   ["module:react-native-dotenv", {
-    //     "moduleName": "@env",
-    //     "path": ".env",
-    //     "blacklist": null,
-    //     "whitelist": null,
-    //     "safe": false,
-    //     "allowUndefined": true
-    //   }],
-    // ],
-    presets: ['babel-preset-expo', 'module:react-native-dotenv'],
+    presets: ['babel-preset-expo'],
   };
 };
